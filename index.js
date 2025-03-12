9c682eb2f4559c74e542170ca9d502c393ec9695
anas126s <anasalsaik16@gmail.com>
2025-03-12 12:24:19 +0000

    web page (index.js)

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 "use strict";
 
-// Include the app.js file.
-// This will run the code.
-console.log("entrypoint");
-const app = require("./app/app.js");
+console.log("Entrypoint started");
+
+// Import the app.js file
+const app = require("./app/app");
+
+// Define the port
+const PORT = process.env.PORT || 3000;
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
